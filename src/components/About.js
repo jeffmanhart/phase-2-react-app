4fa341dcaa892a623525981a17c1e80cce1644b7
jeffmanhart <jeffreymanhart@gmail.com>
2024-01-04 09:51:29 -0700

    cleaned up About Section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 function About() {
-  const bio = "lorem Ipsum"
+  
     return (
     <div id="About">
       <h2>About</h2>
-      {bio && bio.length > 1 ? <p>{bio}</p> : null}
-      <img src="https://media.licdn.com/dms/image/C4E03AQGJx3Z9ZjsRvg/profile-displayphoto-shrink_800_800/0/1571669935914?e=2147483647&v=beta&t=6zQPI7l7Nfym-M_-m8IpxroqIrryQYZxApU6dKH4PDc" alt="Jeffrey Manhart" />
+      <p>This app was made for those who need a little help finding something to do.</p>
+      <h4>Thanks to boredapi.com for providing all the ideas</h4>
+      <p> Built by Jeff Manhart</p>
     </div>
   );
 }
