5cca2a76f3ef012e53def57258264a1c2c8fd9f1
jeffmanhart <jeffreymanhart@gmail.com>
2024-01-04 08:08:43 -0700

    adding the db for saving ideas

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -3,6 +3,7 @@ import React , { useState , useEffect } from 'react';
 
 function Activity() {
   const [activity, setActivity] = useState({});
+  const [saveChecked, setCheck] = useState(false)
 
   useEffect(()=>{
     getRandomActivity()
@@ -20,18 +21,30 @@ function Activity() {
     .then((data)=> setActivity(data));
   }
 
+  function handleSaveCheck(){
+    setCheck(!saveChecked)
+    
+  }
+
   return (
       <main>
         <h1>Bored?!</h1>
         <p>Find something to do, either choose a category or find a random activity</p>
         <Finder onBoredFormSubmit={onBoredFormSubmit} onRandomQuery={getRandomActivity}/>
+        <hr></hr>
         <div id="idea">
           <h3>{activity.activity}</h3>
           <a>{activity.type}</a>
           <p>You can do this activity with {activity.participants === 1 ? "1 person" : activity.participants + " people" }</p>
           <p>Could cost ${activity.price}</p>
           <a href={activity.link}>{activity.link}</a>
-          
+          <label>
+            <input type="checkbox"
+              value={saveChecked}
+              onChange={handleSaveCheck}
+            />
+            {"Save Idea for Later"}
+          </label>
         </div>
       </main>
       
--- src/components/SavedActivities.js
+++ src/components/SavedActivities.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function SavedActivities() {
-    return (
+    
+  
+  return (
     <div id="activities">
       <h2>Saved Activities</h2>
       <ul>
         {
-        //get list of locations from DB
-        //make them clickable to nav back to wether page
+        
         }
       </ul>
     </div>
